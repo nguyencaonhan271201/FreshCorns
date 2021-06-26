0083ec25018271360da00c1d8ec65daedfd4ad49
Nguyễn Cao Nhân <1959024@itec.hcmus.edu.vn>
2021-06-26 13:03:10 +0700

    LandingPage

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include'include/header.php'?>
+
+        <div class="nav_list d-flex align-items-center">
+            <div>
+                <ul>
+                    <li>Login</li>
+                    <li><i class="bi bi-search"></i> Search</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="banner" id="landing">
+        <div class="img_box">
+            <img src="imgs/0.png">            
+        </div>
+
+        <div class="tagline_box">
+            <h2>RT stands for iRrelevanT</h2>
+            <button class ="button-1">Join our comunity</button>
+        </div>
+    </div>   
+
+    <div class="banner d-flex flex-column justify-content-center align-items-center">
+        <h1><u>Constructive</u> criticism. No bullsh*ts.</h1>
+        <h2>A social media platform for those cinephiles who have taste & a common sense.</h2>
+        <h3>No. You may not join us mr Snyder.</h3>
+
+        <div>
+            <i class="bi bi-arrow-up-circle"></i>
+            <i class="bi bi-chat"></i>
+            <i class="bi bi-heart"></i>
+        </div>
+
+        <h1>July 2021</h1>
+    </div>
+
+<?php include'include/footer.php'?>
